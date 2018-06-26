e955664fcbd43173443acafd57edb0af3076b109
jordan-a-young <jordanyoung1616@gmail.com>
2018-06-26 17:02:10 -0400

    Removed Unneeded code

--- src/containers/Results.js
+++ src/containers/Results.js
@@ -4,50 +4,6 @@ import Header from "../components/Header";
 import ResultsTable from "../components/ResultsTable";
 
 class Results extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			rowSelected: false,
-			selected: "",
-			data: [
-				{
-					appID: this.props.values.appID,
-					orgName: this.props.values.name,
-					taxID: this.props.values.taxID,
-					npi: this.props.values.npi,
-					address: this.props.values.address,
-					status: this.props.values.status
-				}
-			],
-			columns: [
-				{
-					dataField: "appID",
-					text: "Application ID"
-				},
-				{
-					dataField: "orgName",
-					text: "Organization Name"
-				},
-				{
-					dataField: "taxID",
-					text: "Tax ID"
-				},
-				{
-					dataField: "npi",
-					text: "NPI"
-				},
-				{
-					dataField: "address",
-					text: "Address"
-				},
-				{
-					dataField: "status",
-					text: "Status"
-				}
-			]
-		};
-	}
-
 	render() {
 		return (
 			<div>

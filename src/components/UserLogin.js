39e3be7fc042adf08ebb24d33a35769bb017e34b
jordan-a-young <jordanyoung1616@gmail.com>
2018-06-27 17:22:26 -0400

    Mobx username
    setUsername now properly updates on change for login

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { AvField, AvForm } from "availity-reactstrap-validation";
 import { FormGroup, Button, Card, CardTitle, CardBody } from "reactstrap";
 import { Redirect } from "react-router-dom";
-import { observer } from "mobx-react";
+import { observer, inject } from "mobx-react";
+import PropTypes from "prop-types";
 
+@inject("appStore")
 @observer
 class UserLogin extends Component {
 	constructor() {
@@ -12,6 +14,20 @@ class UserLogin extends Component {
 			submitted: false
 		};
 	}
+
+	static propTypes = {
+		appStore: PropTypes.shape({
+			state: PropTypes.any
+		}),
+		history: PropTypes.shape({
+			push: PropTypes.func
+		})
+	};
+
+	handleChange = event => {
+		const { appStore } = this.props;
+		appStore.setUsername(event.target.value);
+	};
 
 	handleSubmit = () => {
 		this.setState({ submitted: true });
@@ -42,6 +58,7 @@ class UserLogin extends Component {
 								label="User ID: "
 								type="text"
 								placeholder="User ID"
+								onChange={this.handleChange}
 								required
 							/>
 							<FormGroup>
